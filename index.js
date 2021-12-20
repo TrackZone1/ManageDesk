5427d6e7fa9d3a16a5e8ab564bf4dab547b5401f
TrackZone <48413426+TrackZoneV2@users.noreply.github.com>
2021-12-20 19:11:46 +0100

    Update index.js
    Optimize Code

--- index.js
+++ index.js
@@ -6,203 +6,237 @@ by TrackZone
 Github https://github.com/TrackZoneV2
 
 */
-
 var homeDir = require('os').homedir();
 var fs = require('fs');
 var path = require('path');
 
-var desktopDir = `${homeDir}/Desktop`;
-var documentsDir = `${homeDir}/Documents`;
-var picturesDir = `${homeDir}/Pictures`;
-var musicDir = `${homeDir}/Music`;
-var downloadDir = `${homeDir}/Downloads`;
-var videoDir = `${homeDir}/Videos`;
+var windows_directory_list = [
+
+    {
+        "name": "Desktop",
+        "directory": `${homeDir}/Desktop`
+    },
+    {
+        "name": "Documents",
+        "directory": `${homeDir}/Documents`,
+        "extname_list": [
+
+            "doc",
+            "docx",
+            "odt",
+            "pdf",
+            "ods",
+            "txt",
+
+        ]
+    },
+    {
+        "name": "Pictures",
+        "directory": `${homeDir}/Pictures`,
+        "extname_list": [
+
+            "jpeg",
+            "jpg",
+            "png",
+            "gif",
+
+        ]
+    },
+    {
+        "name": "Music",
+        "directory": `${homeDir}/Music`,
+        "extname_list": [
+
+            "m4a",
+            "flac",
+            "mp3",
+            "wav",
+            "wma",
+            "aac",
+
+        ]
+    },
+    {
+        "name": "Downloads",
+        "directory": `${homeDir}/Downloads`
+    },
+    {
+        "name": "Videos",
+        "directory": `${homeDir}/Videos`,
+        "extname_list": [
+
+            "webm",
+            "mkv",
+            "flv",
+            "vob",
+            "ogv",
+            "ogg",
+            "drc",
+            "gifv",
+            "mng",
+            "avi",
+            "mts",
+            "m2ts",
+            "ts",
+            "mov",
+            "qt",
+            "wmv",
+            "yuv",
+            "rm",
+            "rmvb",
+            "viv",
+            "asf",
+            "mp4",
+            "m4p",
+            "m4v",
+            "mpg",
+            "mp2",
+            "mpeg",
+            "mpe",
+            "mpv",
+            "mpg",
+            "mpeg",
+            "m2v",
+            "svi",
+            "3g2",
+            "mxf",
+            "roq",
+            "nsv",
+            "flv",
+            "f4v",
+            "f4p",
+            "f4a",
+            "f4b"
+
+        ]
+    },
+
+];
 
 var extname_pictures_list = [
 
-	"jpeg",
-	"jpg",
-	"png",
-	"gif",
-	"webp",
+    "jpeg",
+    "jpg",
+    "png",
+    "gif",
 
 ];
 
 var extname_music_list = [
 
-	"m4a",
-	"flac",
-	"mp3",
-	"wav",
-	"wma",
-	"aac",
+    "m4a",
+    "flac",
+    "mp3",
+    "wav",
+    "wma",
+    "aac",
 
 ];
 
 var extname_document_list = [
 
-	"doc",
-	"docx",
-	"odt",
-	"pdf",
-	"ods",
-	"txt",
+    "doc",
+    "docx",
+    "odt",
+    "pdf",
+    "ods",
+    "txt",
 
 ];
 
 var extname_video_list = [
 
-	"webm",
-	"mkv",
-	"flv",
-	"vob",
-	"ogv",
-	"ogg",
-	"drc",
-	"gifv",
-	"mng",
-	"avi",
-	"mts",
-	"m2ts",
-	"ts",
-	"mov",
-	"qt",
-	"wmv",
-	"yuv",
-	"rm",
-	"rmvb",
-	"viv",
-	"asf",
-	"mp4",
-	"m4p",
-	"m4v",
-	"mpg",
-	"mp2",
-	"mpeg",
-	"mpe",
-	"mpv",
-	"mpg",
-	"mpeg",
-	"m2v",
-	"svi",
-	"3g2",
-	"mxf",
-	"roq",
-	"nsv",
-	"flv",
-	"f4v",
-	"f4p",
-	"f4a",
-	"f4b"
+    "webm",
+    "mkv",
+    "flv",
+    "vob",
+    "ogv",
+    "ogg",
+    "drc",
+    "gifv",
+    "mng",
+    "avi",
+    "mts",
+    "m2ts",
+    "ts",
+    "mov",
+    "qt",
+    "wmv",
+    "yuv",
+    "rm",
+    "rmvb",
+    "viv",
+    "asf",
+    "mp4",
+    "m4p",
+    "m4v",
+    "mpg",
+    "mp2",
+    "mpeg",
+    "mpe",
+    "mpv",
+    "mpg",
+    "mpeg",
+    "m2v",
+    "svi",
+    "3g2",
+    "mxf",
+    "roq",
+    "nsv",
+    "flv",
+    "f4v",
+    "f4p",
+    "f4a",
+    "f4b"
 
 ];
 
 fs.readdir(desktopDir, (err, files) => {
-  files.forEach(file => {
-  	if (path.extname(file)){
-  		/*
+    files.forEach(file => {
+        if (path.extname(file)) {
+            /*
 
 			Remove Steam Game Shortcut
 
   		*/
-  		if (path.extname(file).includes('url')){
-  			fs.unlinkSync(desktopDir + "/" + file);
-  			console.log("Remove Steam Game Shortcut =>", file);
-  		}
+            if (path.extname(file).includes('url')) {
+                fs.unlinkSync(desktopDir + "/" + file);
+                console.log("Remove Steam Game Shortcut =>", file);
+            }
 
-  		/*
+            /*
 	
-			Manage Pictures
+			Manage Files 
 
   		*/
 
-  		extname_pictures_list.forEach(extname => {
+            windows_directory_list.forEach(wd => {
 
-  			if (path.extname(file).includes(extname)){
+                if (wd.extname_list) {
 
-					if (!fs.existsSync(picturesDir + "/" + extname.split(".")[0])){
-					    fs.mkdirSync(picturesDir + "/" + extname.split(".")[0]);
-					}
+                    wd.extname_list.forEach(extname => {
 
-		  		fs.rename(desktopDir + "/" + file, picturesDir + "/" + extname.split(".")[0] + "/" + file, function (err) {
-					  if (err) throw err
-					  console.log('Move File to Windows Directory Pictures =>', file)
-					});
+                        if (path.extname(file).includes(extname.toLowerCase())) {
 
-  			}
+                            if (!fs.existsSync(wd.directory + "/" + extname.split(".")[0])) {
+                                fs.mkdirSync(wd.directory + "/" + extname.split(".")[0]);
+                            }
 
-  		});
+                            fs.rename(desktopDir + "/" + file, wd.directory + "/" + extname.split(".")[0] + "/" + file, function(err) {
+                                if (err) throw err
+                                console.log('Move File to Windows Directory ' + wd.name + ' =>', file)
+                            });
 
-  		/*
-	
-			Manage Music
+                        }
 
-  		*/
+                    });
 
-  		extname_music_list.forEach(extname => {
+                }
 
-  		if (path.extname(file).includes(extname)){
+            });
 
-			if (!fs.existsSync(musicDir + "/" + extname.split(".")[0])){
-			    fs.mkdirSync(musicDir + "/" + extname.split(".")[0]);
-			}
-
-  		fs.rename(desktopDir + "/" + file, musicDir + "/" + extname.split(".")[0] + "/" + file, function (err) {
-			  if (err) throw err
-			  console.log('Move File to Windows Directory Music =>', file)
-			});
-
-  		}
-
-  		});
-
-  		/*
-	
-			Manage Documents
-
-  		*/
-
-  		extname_document_list.forEach(extname => {
-
-  		if (path.extname(file).includes(extname)){
-
-			if (!fs.existsSync(documentsDir + "/" + extname.split(".")[0])){
-			    fs.mkdirSync(documentsDir + "/" + extname.split(".")[0]);
-			}
-
-  		fs.rename(desktopDir + "/" + file, documentsDir + "/" + extname.split(".")[0] + "/" + file, function (err) {
-			  if (err) throw err
-			  console.log('Move File to Windows Directory Documents =>', file);
-			});
-
-  		}
-
-  		});
-
-  		/*
-	
-			Manage Video
-
-  		*/
-
-  		extname_video_list.forEach(extname => {
-
-  		if (path.extname(file).includes(extname)){
-
-			if (!fs.existsSync(videoDir + "/" + extname.split(".")[0])){
-			    fs.mkdirSync(videoDir + "/" + extname.split(".")[0]);
-			}
-
-  		fs.rename(desktopDir + "/" + file, videoDir + "/" + extname.split(".")[0] + "/" + file, function (err) {
-			  if (err) throw err
-			  console.log('Move File to Windows Directory Video =>', file);
-			});
-
-  		}
-
-  		});	
-	}
-  });
+        }
+    });
 });
 
 /*
@@ -211,13 +245,16 @@ Clear Downloads Directory
 
 */
 
-fs.readdir(downloadDir, (err, files) => {
-  files.forEach(file => {
-  	if (path.extname(file)){
-  		fs.unlinkSync(downloadDir + "/" + file);
-  		console.log("Delete File =>", file);
-  	}else{
-  		fs.rmSync(downloadDir + "/" + file, { recursive: true, force: true });
-  	}
-  });
+fs.readdir(windows_directory_list[4].directory, (err, files) => {
+    files.forEach(file => {
+        if (path.extname(file)) {
+            fs.unlinkSync(windows_directory_list[4].directory + "/" + file);
+            console.log("Delete File =>", file);
+        } else {
+            fs.rmSync(windows_directory_list[4].directory + "/" + file, {
+                recursive: true,
+                force: true
+            });
+        }
+    });
 });
